1b3340704ab72c41583f771d8552febecc7020a8
Chintapallidharmendra <chintapallidharmendra@gmail.com>
2024-11-05 21:26:28 +0530

    Deployment fixes

--- src/utils/const.js
+++ src/utils/const.js
@@ -2,7 +2,7 @@ let _SERVER_BACKEND_URL = "http://127.0.0.1:8000/api/";
 
 if (process.env.NODE_ENV === "production") {
   // TODO: Update the production URL
-  _SERVER_BACKEND_URL = "http://3.110.27.238:8000/api/";
+  _SERVER_BACKEND_URL = "https://statustracker.publicvm.com/api/";
 }
 
 export const SERVER_BACKEND_URL = _SERVER_BACKEND_URL;
